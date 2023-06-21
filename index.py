951135092e55ce843806fc96e7f96368167a304b
iamyashchouhan <48046446+iamyashchouhan@users.noreply.github.com>
2023-06-21 19:17:51 +0530

    Update main.py

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
-from flask import Flask
-from flask_ngrok import run_with_ngrok
-app = Flask(__name__)
-run_with_ngrok(app)
+from flask import Flask, render_template, request
+import gpt4all
 
-@app.route("/")
+app = Flask(__name__, static_folder='static')
+app.config['SECRET_KEY'] = 'secret_key'
+
+PATH = '/Users/ymg/Library/Application Support/nomic.ai/GPT4All/GPT4All-13B-snoozy.ggmlv3.q4_0.bin'
+mpt = gpt4all.GPT4All(PATH, model_type='mpt')
+
+@app.route('/')
 def home():
-	return "<h1>GFG is great platform to learn</h1>"
-	
-app.run()
+    return render_template('index.html')
+
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    user_input = request.form['user_input']
+    message = [{"role": "user", "content": user_input}]
+    response = mpt.chat_completion(message)
+    content = response['choices'][0]['message']['content']
+    return content
+
+if __name__ == '__main__':
+    app.run(debug=True)
